14c90eac89a24e6d740be70108cfa1adac5b1b64
Mark Crisostomo <mark@deviseinteractive.com>
2017-12-06 22:16:05 -0800

    added portfolio hbs

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.get('/about', (req, res) => {
     })
 });
 
+app.get('/portfolio', (req, res) => {
+    res.render('portfolio.hbs', {
+        pageTitle: 'Portfolio Page',
+        pageContent: 'Portfolio content'
+    })
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'unable to handle request',
